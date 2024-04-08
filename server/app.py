261ed07ae3913d7d238cfb0620a7093d8eaf2bce
neuman <owiroadams@gmail.com>
2024-04-08 11:26:58 +0300

    completed

--- server/app.py
+++ server/app.py
@@ -2,7 +2,40 @@
 
 from flask import Flask
 
+
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:param>')
+def print_string(param):
+    print(param)
+    return param
+
+@app.route('/count/<int:param>')
+def count(param):
+    equals = ""
+    for num in range(param):
+        equals = equals + f'{num}\n'
+    return equals
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        result = num1 / num2
+    elif operation == '%':
+        result = num1 % num2
+    else:
+        return 'Invalid operation'
+    return str(result)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
